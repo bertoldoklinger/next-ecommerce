38aaa4ac8681c3493ab971c5cd93b793c66aacd3
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-09-17 22:02:47 -0300

    feat: add next,prisma and nextauth with google provider and stripe integration

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+
+  return (
+    <main className="text-lg">
+      <h1>Hello Bertoldo Klinger</h1>
+      
+    </main>
+  )
+}
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,38 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import Stripe from 'stripe'
+
+const prisma = new PrismaClient()
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  events: {
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: "2022-11-15"
+      })
+      if (user.email && user.name) {
+        const costumer = await stripe.customers.create({
+          email: user.email,
+          name: user.name,
+        })
+        await prisma.user.update({
+          where: {
+            id: user.id
+          },
+          data: {
+            stripeCostumerId: costumer.id
+          }
+        })
+      }
+    },
+  }
+})
